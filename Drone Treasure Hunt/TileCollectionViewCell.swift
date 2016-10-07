6a14991fada4423117ca3c30692c89b6b0814b09
Massimo Donati <mass.donati@gmail.com>
2016-10-06 22:31:11 -0700

    grid in place

---
+++ Drone Treasure Hunt/TileCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TileCollectionViewCell.swift
+//  Drone Treasure Hunt
+//
+//  Created by Massimo Donati on 10/6/16.
+//  Copyright © 2016 Massimo Donati. All rights reserved.
+//
+
+import UIKit
+
+class TileCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "tileCell"
+
+    @IBOutlet weak var tilePaintView: UIView!
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.tilePaintView.layer.cornerRadius = self.frame.width / 2
+    }
+}
--- Drone Treasure Hunt/ViewController.swift
+++ Drone Treasure Hunt/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let colsNumber = 5
+    let rowsNumber = 5
+    var defaultGap: CGFloat = 0.0
+
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
-
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        switch self.traitCollection.userInterfaceIdiom {
+        case .phone:
+            defaultGap = 10
+            self.collectionView.collectionViewLayout.invalidateLayout()
+            return
+        case .pad:
+            defaultGap = 40
+            self.collectionView.collectionViewLayout.invalidateLayout()
+            return
+        default:
+            defaultGap = 0.0
+        }
+    }
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colsNumber * rowsNumber
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TileCollectionViewCell.identifier, for: indexPath)
+        return cell;
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: defaultGap, bottom: 0, right: defaultGap)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return defaultGap
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return defaultGap
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cvWidth = collectionView.frame.width
+        // totalGap is 2 * defaultGap for the left and right insets + 4 * defaultGap for each space
+        // between the items in the same line
+        let totalGap = 2 * defaultGap + CGFloat(colsNumber + -1) * defaultGap
+        let itemWidth = (cvWidth - totalGap) / CGFloat(colsNumber)
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+}
