4a468d320906f54dd40cdb700491281817a61a12
Massimo Donati <mass.donati@gmail.com>
2016-10-07 13:45:32 -0700

    added Placeable protocol

---
+++ Drone Treasure Hunt/Placeable.swift
@@ -0,0 +1,14 @@
+//
+//  Positionable.swift
+//  Drone Treasure Hunt
+//
+//  Created by Massimo Donati on 10/7/16.
+//  Copyright © 2016 Massimo Donati. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Placeable {
+    func put(in place: UIView!)
+}
